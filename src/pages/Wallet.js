94202809141ea6a83f4567b16303b4054f52fddf
Victor Murilo <victor2706.vm@gmail.com>
2022-07-29 14:21:11 -0300

    Cumpri o requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
     return (
       <>
         <Header />
+        <Table />
         <WalletForm />
       </>
     );
